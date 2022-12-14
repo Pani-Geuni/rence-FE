eee19ed0e8ffff75939a687a6dc8d4910bb4603a
PanGeun <lifebook0809@gmail.com>
2022-12-14 13:47:57 +0900

    [feat] Vue3 Datepicker 추가

--- test_router/src/main.js
+++ test_router/src/main.js
@@ -2,10 +2,14 @@
 import { createApp } from 'vue';
 import VueCookies from 'vue-cookies';
 import Vue3Storage from 'vue3-storage';
+import Datepicker from '@vuepic/vue-datepicker';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+
 createApp(App).use(store).use(router).use(VueCookies)
   .use(Vue3Storage)
+  .component('Datepicker', Datepicker)
   .mount('#app');
