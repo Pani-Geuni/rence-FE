65b35b3030507ccebaa99ed4c7210899c2805442
PanGeun <lifebook0809@gmail.com>
2022-12-18 19:44:40 +0900

    [remove] 레거시 JS 파일 제거

--- test_router/src/assets/JS/backoffice/settings.js
+++
@@ -1,8 +0,0 @@
-/**
-* @author : 전판근, 김예은
-*/
-$(function() {
-	$('#btn-update-host').click(function() {
-		location.href = '/backoffice/update_host?backoffice_no=' + $.cookie("backoffice_no");
-	})
-})
